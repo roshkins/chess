e34443f159069430628b64ba34bbed56ad8468bf
App Academy Student <student@appacademy.io>
2013-07-23 11:37:55 -0700

    added DIRECTIONS constant in Slide and initialize in Slide

--- lib/basic_pieces.rb
+++ lib/basic_pieces.rb
@@ -1,14 +1,25 @@
 class Piece
   attr_reader :position
+
   def initialize(position, board)
     @position, @board = position, board
   end
-end
-
-class Step
 
 end
 
-class King
+class Slide < Piece
+  DIRECTIONS = [:up, :down, :left, :right, :up_left, :up_right, :down_left, :down_right]
+
+  def initialize(position, board)
+    super(position, board)
+  end
+
+  def move(direction, length)
+
+  end
+
+end
+
+class King < Slide
 
 end
