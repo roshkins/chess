d17398a000f7f10f36c8cdc69fa9884587928dcd
App Academy Student <student@appacademy.io>
2013-07-23 11:55:22 -0700

    Added move methods to Slide and King

--- lib/basic_pieces.rb
+++ lib/basic_pieces.rb
@@ -15,16 +15,54 @@ class Slide < Piece
   end
 
   def move(direction, length)
+    raise RuntimeError.new("not a valid direction") unless DIRECTIONS.include?(direction)
     self.send(direction)
   end
 
   private
   def up_left(length)
+    @position.first -= length
+    @position.last  += length
+  end
 
+  def up(length)
+    @position.last  += length
+  end
 
+  def up_right(length)
+    @position.first += length
+    @position.last  += length
+  end
+
+  def right(length)
+    @position.first += length
+  end
+
+  def down_right(length)
+    @position.first += length
+    @position.last  -= length
+  end
+
+  def down(length)
+    @position.first -= length
+  end
+
+  def down_left(length)
+    @position.first -= length
+    @position.last  -= length
+  end
+
+  def left(length)
+    @position.first -= length
   end
 end
 
 class King < Slide
+  def initialize(position, board)
+    super(position, board)
+  end
 
+  def move(direction)
+    super(direction, 1)
+  end
 end
